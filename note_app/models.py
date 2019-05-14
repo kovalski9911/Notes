5daaee38f3a70105f32e2d6de311ba61a0502ee6
kovalski9911 <4str90@gmail.com>
2019-05-14 17:40:18 +0300

    added Note and Category models

---
+++ note_app/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Category(models.Model):
+    """Model of category"""
+
+    name = models.CharField('category name', max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Note(models.Model):
+    """Model of note"""
+
+    name = models.CharField('note name', max_length=100)
+    content = models.TextField('note content')
+    category = models.ManyToManyField(Category)
+    created = models.DateTimeField(auto_now_add=True)
+    # author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
